221b0a5ef9f3c1344da13180197c981b62101fd8
Dondelium <Dondelium@gmail.com>
2020-08-16 23:26:11 -0400

    Updates

--- schematicRecipeMaker.js
+++ schematicRecipeMaker.js
@@ -7,16 +7,16 @@ fs.readFile(`Z:/Steam/steamapps/common/7 Days To Die/Data/Config/items.xml`, "ut
   
   lines = data.split('\n');
   for(var i in lines){
-	//Code to handle commented sections.
-	if(!commented && lines[i].indexOf("<!--") != -1) commented = true;
-    if(commented && lines[i].indexOf("-->") != -1){commented = false; continue;}
-	if(commented) continue;
+    //Code to handle commented sections.
+    if(!commented && lines[i].indexOf("<!--") != -1) commented = true;
+      if(commented && lines[i].indexOf("-->") != -1){commented = false; continue;}
+    if(commented) continue;
 
-	//Read schematics, create xml needed, and add to list.
+    //Read schematics, create xml needed, and add to list.
     if(lines[i].indexOf('Schematic">') == -1) continue;
-	var schematicName = lines[i].split('"')[1];
-	
-	newFile.push(`\t\t<recipe name="${schematicName}" count="2"><ingredient name="resourcePaper" count="5"/><ingredient name="resourceCoal" count="1"/><ingredient name="${schematicName}" count="1"/></recipe>`);
+    var schematicName = lines[i].split('"')[1];
+    
+    newFile.push(`\t\t<recipe name="${schematicName}" count="2"><ingredient name="resourcePaper" count="5"/><ingredient name="resourceCoal" count="1"/><ingredient name="${schematicName}" count="1"/></recipe>`);
   }
   newFile.push(`\t</append>\n</configs>`);
 
